54c7ce42b319ac748d57c4d06e1ed6916cc81d1c
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-04 22:23:49 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 const ContactList = ({ children }) => (
-	<>
+	<> 
 		<ul className={css.contactsList}>
 			{children}
 		</ul>
