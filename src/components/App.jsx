4b78e36755fe3d436255cf34fad2e649acd716d2
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-04 23:44:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,16 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import ContactItem from './ContactItem/ContactItem';
 
-
-class App extends Component { 
+class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: ''
+    contacts: [],
+    filter: '',
   };
 
-  // Xранение контактов телефонной книги в localStorage
+  // Xранение контактов телефонной книги в localStorage================
   componentDidUpdate(prevState, prevProps) {
     if (this.state.contacts !== prevState.contacts) {
+      console.log('oбновилось поле contacts');
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
@@ -33,9 +28,9 @@ class App extends Component {
     }
   }
 
+  ////==========================================================================================
 
-
-  addContacts = data => { 
+  addContacts = data => {
     const { contacts } = this.state;
     const names = contacts.map(contact => contact.name.toLowerCase());
 
@@ -44,7 +39,7 @@ class App extends Component {
       ? alert(`${data.name} is already in contact`)
       : this.setState(prevState => ({
           contacts: [data, ...prevState.contacts],
-      }))
+        }));
   };
 
   deleteContact = contactId => {
@@ -62,26 +57,28 @@ class App extends Component {
     const normalizeFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter),
+      contact.name.toLowerCase().includes(normalizeFilter)
     );
   };
-  
+
   render() {
     const { filter } = this.state;
     const visibleContact = this.getVisibleContact();
 
     return (
       <>
-          <div className={s.container}>
-            <h1 className={s.title}>Phonebook</h1>
-            <ContactForm onSubmit={this.addContacts} />
-            <h2 className={s.title}>Contacts</h2>
-            <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList>
-                <ContactItem contacts={visibleContact}
-                onDeleteContact={this.deleteContact}/>
-            </ContactList>
-          </div>
+        <div className={s.container}>
+          <h1 className={s.title}>Phonebook</h1>
+          <ContactForm onSubmit={this.addContacts} />
+          <h2 className={s.title}>Contacts</h2>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList>
+            <ContactItem
+              contacts={visibleContact}
+              onDeleteContact={this.deleteContact}
+            />
+          </ContactList>
+        </div>
       </>
     );
   }
