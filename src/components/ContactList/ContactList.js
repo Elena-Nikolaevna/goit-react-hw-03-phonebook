8757b46faddae763c110d1eb93070ee2d9f580cf
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-11 22:59:53 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,16 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
-const ContactList = ({ children }) => (
+const ContactList = ({ contacts, onDeleteContact }) => (
 	<> 
-		<ul className={css.contactsList}>
+	{contacts.map(({ id, name, number }) => (
+		<li key={id} className={css.contactsItem}>
+			<p className={css.contactText}>
+				{name}: {number}
+			</p>
+			<button
+				className={css.button}
+				type='button'
+				onClick={() => onDeleteContact(id)}>
+				Delete
+			</button>
+		</li>
+	))}
+		{/* <ul className={css.contactsList}>
 			{children}
-		</ul>
+		</ul> */}
 	</>
 );
 
 ContactList.propTypes = {
-    children: PropTypes.node.isRequired,
+	contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+    
 };
 
 export default ContactList;
+
+/* export const ContactsList = ({ foundContact, deleteContact }) => {
+  return (
+    <ul>
+      {foundContact.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <p>{name}</p>
+            <p>{number}</p>
+
+            <button type="button" onClick={() => deleteContact(id)}>
+              delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ContactsList.propTypes = {
+  foundContact: PropTypes.arrayOf(
+    PropTypes.objectOf(PropTypes.string.isRequired).isRequired
+  ).isRequired,
+  deleteContact: PropTypes.func.isRequired,
+}; */
